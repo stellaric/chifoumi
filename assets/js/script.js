447444b4bbc32b9a96f51d4f49c9c8fc9b759e1c
stellaric <stella.richal.sio@gmail.com>
2023-07-10 22:57:16 +0200

    v1 finale

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,128 @@ function validateForm(event) {
         document.getElementById("contactForm").reset();
     }
 }
+
+//test
+// script.js
+
+// script.js
+
+var roundsPlayed = 0;
+var playerScore = 0;
+var computerScore = 0;
+
+function startNewGame() {
+    roundsPlayed = 0;
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById("round").textContent = roundsPlayed;
+    document.getElementById("computer_score").textContent = computerScore;
+    document.getElementById("player_score").textContent = playerScore;
+    document.getElementById("result").textContent = "";
+    document.getElementById("winner").textContent = "";
+    document.getElementById("ciseauBtn").disabled = false;
+    document.getElementById("pierreBtn").disabled = false;
+    document.getElementById("feuilleBtn").disabled = false;
+    newSound = document.getElementById("newSound");
+    newSound.play();
+
+}
+
+function generateComputerChoice() {
+    var choices = ["pierre", "feuille", "ciseau"];
+    var randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function compareChoices(playerChoice, computerChoice) {
+    var resultElement = document.getElementById("result");
+    var roundElement = document.getElementById("round");
+    var computerScoreElement = document.getElementById("computer_score");
+    var playerScoreElement = document.getElementById("player_score");
+
+    if (playerChoice === computerChoice) {
+        resultElement.textContent = "Égalité !";
+    } else if (
+        (playerChoice === "pierre" && computerChoice === "ciseau") ||
+        (playerChoice === "feuille" && computerChoice === "pierre") ||
+        (playerChoice === "ciseau" && computerChoice === "feuille")
+    ) {
+        resultElement.textContent = "Vous avez gagné !";
+        playerScore++;
+    } else {
+        resultElement.textContent = "L'ordinateur a gagné !";
+        computerScore++;
+
+    }
+
+    roundsPlayed++;
+    roundElement.textContent = roundsPlayed;
+    computerScoreElement.textContent = computerScore;
+    playerScoreElement.textContent = playerScore;
+
+    if (roundsPlayed === 3) {
+        if (playerScore > computerScore) {
+            document.getElementById("winner").textContent = "Vous avez gagné la partie !";
+            // Jouer le son de fin de jeu
+            var winnerSound = document.getElementById("winnerSound");
+            winnerSound.play();
+        } else if (playerScore < computerScore) {
+            document.getElementById("winner").textContent = "L'ordinateur a gagné la partie !";
+            // Jouer le son de défaite
+            var loseSound = document.getElementById("loseSound");
+            loseSound.play();
+        } else {
+            document.getElementById("winner").textContent = "La partie est terminée avec une égalité !";
+            // Jouer le son de fin de jeu
+            var loseSound = document.getElementById("loseSound");
+            loseSound.play();
+        }
+
+
+        // Désactiver les boutons après la fin du jeu
+
+        document.getElementById("ciseauBtn").disabled = true;
+        document.getElementById("pierreBtn").disabled = true;
+        document.getElementById("feuilleBtn").disabled = true;
+    }
+}
+
+function play(choice) {
+    var playerChoiceImage = document.getElementById("player-choice");
+    switch (choice) {
+        case "pierre":
+            playerChoiceImage.src = "../src/img/main/pierre.png";
+
+            break;
+        case "feuille":
+            playerChoiceImage.src = "../src/img/main/feuille.png";
+
+            break;
+        case "ciseau":
+            playerChoiceImage.src = "../src/img/main/ciseau.png";
+
+            break;
+        default:
+            playerChoiceImage.src = "";
+            break;
+    }
+
+    var computerChoice = generateComputerChoice();
+    var computerChoiceImage = document.getElementById("computer-choice");
+    switch (computerChoice) {
+        case "pierre":
+            computerChoiceImage.src = "../src/img/main/pierre.png";
+            break;
+        case "feuille":
+            computerChoiceImage.src = "../src/img/main/feuille.png";
+            break;
+        case "ciseau":
+            computerChoiceImage.src = "../src/img/main/ciseau.png";
+            break;
+        default:
+            computerChoiceImage.src = "";
+            break;
+    }
+
+    compareChoices(choice, computerChoice);
+}
